2fbb76d12df34c98a441d72c8af7ccef20155aba
kyle <kyle.castillon.dev@gmail.com>
2024-03-08 06:55:43 +0000

    removed default behavoir for tab and enter

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,4 +1,8 @@
 local opt = vim.opt --for conciseness
+local api = vim.api
+
+api.nvim_set_keymap('n', '<Tab>', '<Nop>', { noremap = true })
+api.nvim_set_keymap('n', '<CR>', '<Nop>', { noremap = true })
 
 -- line numbers
 opt.relativenumber = true
@@ -51,5 +55,4 @@ opt.swapfile = false
 
 opt.scrolloff = 15
 opt.colorcolumn = '80'
-opt.updatetime = 50
 opt.linespace = 15
